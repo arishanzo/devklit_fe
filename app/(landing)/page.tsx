06a03f91628f8ca2cc586963820323a0f60a745d
Your Name <you@example.com>
2026-03-09 22:05:01 +0700

    uo

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,6 @@ import CompanyProfile from "./components/companyprofile";
 import Solusi from "./components/solusi";
 import Testimoni from "./components/testimoni"
 
-export const runtime = 'edge';
 
 export default function Home() {
   return (
