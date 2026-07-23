9adedfb129e7b356be7489551c362e8d8c9fa1d1
arishanzo <arishanzo86@gmail.com>
2026-07-23 22:14:31 +0700

    fix: Update title in metadata for consistency and branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: {
-    default: "DEVKLIT TECHNOLOGY - JASA PEMBUATAN WEBSITE LAMONGAN",
+    default: "JASA PEMBUATAN WEBSITE LAMONGAN",
     template: "%s | Devklit Tech",
   },
   description:
